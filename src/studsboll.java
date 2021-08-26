f2b39258811708a870e6a4a671a37fc81f27a320
Skalmanleif <isak.thyrel@elev.ga.ntig.se>
2021-08-26 13:38:03 +0200

    Skapat lite variabler

--- src/studsboll.java
+++ src/studsboll.java
@@ -1,5 +1,12 @@
 public class studsboll {
     public static void main(String[] args) {
-        
+        double t = 0;
+        double h = 0;
+        double a = 9.82;
+        while (t<=10) {
+            t+=0.1;
+            h += a;
+            System.out.println(t + " "+ h);
+        }
     }
 }
