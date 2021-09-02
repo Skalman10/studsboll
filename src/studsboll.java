8f001a080e59ff3e896436b8262f5b40e761da64
Skalmanleif <isak.thyrel@elev.ga.ntig.se>
2021-09-02 13:18:24 +0200

    Har skapat några metoder för att uppdatera hastigheten, höjden och kolla studs.

--- src/studsboll.java
+++ src/studsboll.java
@@ -1,12 +1,31 @@
 public class studsboll {
     public static void main(String[] args) {
-        double t = 0;
-        double h = 0;
-        double a = 9.82;
-        while (t<=10) {
-            t+=0.1;
-            h += a;
+        double h = 100;
+        double a = -9.82;
+        double v = 0;
+        for (double t =0;t<=10;t+=0.1) {
             System.out.println(t + " "+ h);
+            v = updateVelocity(v,a);
+            h = updateHeight(h,v);
+            if (checkHeight(h)) {
+                v = -v;
+            }
         }
+    }
+
+    private static boolean checkHeight(double h) {
+        if (h<=0||h>=100) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    private static double updateHeight(double h, double v) {
+        return h+v;
+    }
+
+    private static double updateVelocity(double v, double a) {
+        return v + a*0.1;
     }
 }
