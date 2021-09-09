5a9d1793f87aafa095a7639786f13525a494f129
Skalmanleif <isak.thyrel@elev.ga.ntig.se>
2021-09-09 13:35:57 +0200

    Försökte få studsen av aldrig gå under 0, fick inte att funka

--- src/studsboll.java
+++ src/studsboll.java
@@ -6,15 +6,32 @@ public class studsboll {
         for (double t =0;t<=10;t+=0.1) {
             System.out.println(t + " "+ h);
             v = updateVelocity(v,a);
-            h = updateHeight(h,v);
-            if (checkHeight(h)) {
-                v = -v;
+            if (checkHeight(h,v)) {
+                h = updateHeightBounce(h,v,a);
+                v = updateVelocityBouce(h,v,a);
+            } else {
+                h = updateHeight(h,v);
             }
         }
     }
 
-    private static boolean checkHeight(double h) {
-        return h <= 0 || h >= 100;
+    private static double updateVelocityBouce(double h, double v, double a) {
+        double deltaT = -h-v/(a);
+        v = v+a*deltaT;
+        return -v;
+    }
+
+    private static double updateHeightBounce(double h, double v, double a) {
+        double deltaT = -h-v/(a);
+        v = v+a*deltaT;
+        v = -v;
+        deltaT = 0.1-deltaT;
+        h=0;
+        return h+(v*deltaT);
+    }
+
+    private static boolean checkHeight(double h, double v) {
+        return h+v <= 0;
     }
 
     private static double updateHeight(double h, double v) {
